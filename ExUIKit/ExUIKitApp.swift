cf3a3df835d2ea8541b7c6d316ee3347d22cbc59
jake <palatable7@naver.com>
2022-07-13 23:55:42 +0900

    Initial Commit

---
+++ ExUIKit/ExUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExUIKitApp.swift
+//  ExUIKit
+//
+//  Created by 김종권 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct ExUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
